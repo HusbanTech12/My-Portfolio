dd9c99bd629983d2ef8c1318e9b54123b460112c
HusbanTech12 <husbantech08@gmail.com>
2025-06-02 22:27:58 +0500

    initail commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className='grid grids-cols-1 lg:grid-cols-12'>
+
+      <h1 className='text-white text-4xl lg:text-6xl font-extrabold'>Hello, I'm Husban</h1>
+      <p className='text-white font-medium mt-5 text-lg lg:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta veniam sunt voluptatum beatae adipisci ullam laborum, maiores enim aperiam voluptatibus autem eveniet quo consectetur cum temporibus! Sunt accusantium animi ratione?</p>
+      </div>
+    </section>  
+  )
+}
+
+export default HeroSection
