2bc57f8b62860857bbb0f28d653650db04f34f0d
HusbanTech12 <husbantech08@gmail.com>
2025-06-04 22:25:30 +0500

    Half-Complete Navbar

---
+++ src/app/components/NavLink.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+const NavLink = ({href , title}) =>{
+    return(
+       
+        <Link href={href}
+
+        className='block sm:text-xl rounded md:p-0'>
+            {title}
+       
+       </Link>
+
+    )
+}
+
+export default NavLink
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Link from 'next/link'
+import NavLink from './NavLink'
+
+  const NavLinks = [
+    {
+      title: 'About',
+      path: '#about'
+    },
+
+    {
+      title: 'Projects',
+      path: '#project'
+    },
+
+    {
+      title: 'Contact',
+      path: '#contact'
+    },
+
+
+  ] 
 export default function Navbar() {
+  
   return (
 
-    <nav>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-black bg-opacity-90'>
         <div className='flex flex-wrap items-center justify-between mx-auto  '>
-            <Link href={'/'} className='text-white text-5xl'>
+            <Link href={'/'} className=' mx-5 text-2xl md:text-5xl text-white font-semibold'>
             Husban
             </Link>
         
-         <div className=' manu hidden md:block md:w-auto text-slate-100' id='navbar'>
-           <ul>
-            <li><Link href={'/'} className='block sm:text-xl'>Home</Link></li>
+         <div className=' manu hidden md:block md:w-auto text-slate-100' id='navbar' >
+           <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 '>
+             {
+              NavLinks.map((link,index) =>(
+              <li key={index}>
+                <NavLink  href={link.path} title={link.title}  />
+
+
+              </li>
+
+              )
+              
+              
+              
+              
+              )
+             }
             
            </ul> 
             
